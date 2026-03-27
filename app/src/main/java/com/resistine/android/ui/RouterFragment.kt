d529106f670ce0ef822bc0a0c0d4937062dc895e
feel <mahmoudelfeelig@gmail.com>
2026-03-27 14:39:00 +0100

    remove welcome screen if user has already completed registration or skipped it before

--- app/src/main/java/com/resistine/android/ui/RouterFragment.kt
+++ app/src/main/java/com/resistine/android/ui/RouterFragment.kt
@@ -6,15 +6,19 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.resistine.android.R
 import com.resistine.android.security.CryptoManager
+import com.resistine.android.ui.login.LoginViewModel
 
 class RouterFragment : Fragment(R.layout.fragment_router) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val isLoggedIn = CryptoManager.isConfigStored(requireContext())
+        val context = requireContext()
+        val hasStoredRegistration = CryptoManager.isConfigStored(context) ||
+            CryptoManager.loadDecryptedEmail(context) != null
+        val hasCompletedWelcomeFlow = LoginViewModel.hasCompletedWelcomeFlow(context)
 
-        if (isLoggedIn) {
+        if (hasStoredRegistration || hasCompletedWelcomeFlow) {
             findNavController().navigate(R.id.action_router_to_home)
         } else {
             findNavController().navigate(R.id.action_router_to_welcome)
